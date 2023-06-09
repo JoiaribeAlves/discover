260b84c97da7f67d341ef2fe450dd284492d93c6
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2023-06-09 02:36:17 -0400

    change the avatar

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function toggleMode() {
   const body = document.querySelector("body");
   body.classList.toggle("light");
 
-  const img = document.querySelector("#profile img");
-  if (body.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png");
-  } else {
-    img.setAttribute("src", "./assets/avatar.png");
-  }
+  // const img = document.querySelector("#profile img");
+  // if (body.classList.contains("light")) {
+  //   img.setAttribute("src", "./assets/avatar-light.png");
+  // } else {
+  //   img.setAttribute("src", "./assets/avatar.png");
+  // }
 }
